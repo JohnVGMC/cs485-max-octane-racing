098a37276c087b88842878d7c35a78a8e31926ad
John Calderon <johncalderonvgc@hotmail.com>
2017-11-03 11:52:24 -0700

    Race Course 1 Demonstration

---
+++ Max Octane Racing/Assets/Scripts/Car Script/Car Script.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function Update () 
+{
+	if(Input.GetKey(KeyCode.DownArrow))
+		transform.Translate(Vector3(0,0,-1));
+
+	if(Input.GetKey(KeyCode.UpArrow))
+		transform.Translate(Vector3(0,0,1));
+
+	if(Input.GetKey(KeyCode.LeftArrow))
+		transform.Translate(Vector3(-1,0,0));
+
+	if(Input.GetKey(KeyCode.RightArrow))
+		transform.Translate(Vector3(1,0,0));
+}
---
+++ Max Octane Racing/Assets/Scripts/Car Script/Rotating Wheels.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function Update () {
+	if(Input.GetKey(KeyCode.UpArrow))
+		transform.Rotate(Vector3(20,0,0));
+
+	if(Input.GetKey(KeyCode.DownArrow))
+		transform.Rotate(Vector3(-20,0,0));
+}
